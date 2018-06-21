d73a4b459a23e358f9132d72c4a5b079860f38ed
trainorp <patrick.trainor@louisville.edu>
2018-06-20 22:34:59 -0400

    idk

--- model.R
+++ model.R
@@ -1,7 +1,11 @@
+############ Prereqs ############
 oldPar<-par()
 options(stringsAsFactors=FALSE)
 library(tidyverse)
+library(rstan)
+library(brms)
 
+############ Import annotation and metabolite data ############
 setwd("~/gdrive/AthroMetab/Data")
 spec<-read.csv("AthroACSRawSpectra.csv")
 key<-read.csv("metabolite_key2.csv")
@@ -15,11 +19,14 @@ getMZ<-function(x){
 }
 key$peakMZ<-unlist(lapply(lapply(key$spectra,getMZ),function(x) ifelse(is.null(x),NA,x)))
 
-############ Import ############
 metab<-read.csv("scaled.csv")
+metab$ptid<-as.character(metab$ptid)
 pheno<-metab %>% filter(timepoint=="T0") %>% select(ptid,group)
-metab<-metab %>% filter(timepoint=="T0") %>% select(-ptid,-group,-timepoint) 
+metab<-metab %>% filter(timepoint=="T0") %>% select(-ptid,-group,-timepoint)
+metab<-metab %>% log2()
+rownames(metab)<-pheno$ptid
 
+############ Load feature selection data ############
 load("~/gdrive/AthroMetab/WoAC/outs.RData")
 weights<-c()
 for(i in 1:length(outs)){
@@ -36,3 +43,28 @@ key<-key %>% arrange(as.numeric(gsub("M","",id)))
 weights<-apply(weights,1,function(x) mean(x))
 key$weights<-0
 key$weights[match(names(weights),key$id)]<-weights
+include<-key %>% filter(weights>.025)
+metab2<-metab[,names(metab) %in% include$id]
+
+############ Model ############
+metab2$ptid<-rownames(metab2)
+metab2<-metab2 %>% left_join(pheno)
+
+priors<-get_prior(group~.,data=metab2[,names(metab2)!="ptid"],family="categorical")
+
+ptm<-proc.time()
+brm1<-brm(group~.,data=metab2[,names(metab2)!="ptid"],
+        family="categorical",chains=2,iter=5000,algorithm="sampling")
+proc.time()-ptm
+launch_shinystan(brm1)
+
+ptm<-proc.time()
+prior2<-c(prior(normal(0,10),class=b),
+          prior(normal(0,1),class=b,coef=muType2MI_M133),
+          prior(normal(1,2),class=Intercept))
+priors$prior[5]<-"exponential(1)"
+brm2<-brm(group~.,data=metab2[,names(metab2)!="ptid"],
+          family="categorical",chains=2,iter=500,algorithm="sampling",
+          prior=priors)
+proc.time()-ptm
+launch_shinystan(brm1)
