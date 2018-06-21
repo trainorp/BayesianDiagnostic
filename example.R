5bd17702a0fa93b9ef1a67e950decee5504020da
trainorp <patrick.trainor@louisville.edu>
2018-06-20 21:24:38 -0400

    idk

--- example.R
+++ example.R
@@ -1,1 +1,17 @@
 library(tidyverse)
+library(rstan)
+library(brms)
+
+rstan_options(auto_write=TRUE)
+options(mc.cores=parallel::detectCores()) # Run on multiple cores
+
+set.seed(3875)
+
+ir<-data.frame(scale(iris[,-5]),Species=iris[,5])
+
+ptm<-proc.time()
+b1<-brm(Species~Petal.Length+Petal.Width+Sepal.Length+Sepal.Width,data=ir,
+        family="categorical",chains=4,iter=2000)
+proc.time()-ptm
+
+launch_shinystan(b1)
